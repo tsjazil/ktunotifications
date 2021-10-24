c66dd4f4ace90ce4d0aeb8e133a2fdf82f636736
Jazil T S <tsjazil@gmail.com>
2021-10-24 10:48:48 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-sudo wget -O /usr/local/bin/ktunotif-dmenu https://raw.githubusercontent.com/tsjazil/ktunotifications/main/ktunotif-dmenu
-sudo wget -O /usr/local/bin/ktunotif-fzf https://raw.githubusercontent.com/tsjazil/ktunotifications/main/ktunotif-fzf
+sudo wget -q -O /usr/local/bin/ktunotif-dmenu https://raw.githubusercontent.com/tsjazil/ktunotifications/main/ktunotif-dmenu
+sudo wget -q -O /usr/local/bin/ktunotif-fzf https://raw.githubusercontent.com/tsjazil/ktunotifications/main/ktunotif-fzf
 echo Installing ...
 cd /usr/local/bin
 sudo chmod +x ktunotif-fzf 
