18f136279ae47627ef14dc0938b08edc52f22729
tsjazil <tsjazil@gmail.com>
2021-10-24 10:03:12 +0530

    Added Install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+sudo wget -O /usr/local/bin/ktu-dmenu https://raw.githubusercontent.com/tsjazil/ktunotifications/main/ktunotifications-dmenu
+sudo wget -O /usr/local/bin/ktu-dmenu https://raw.githubusercontent.com/tsjazil/ktunotifications/main/ktunotifications-fzf
+echo Installing ...
+cd /usr/local/bin
+sudo chmod +x ktunotifications-fzf 
+sudo chmod +x ktunotifications-dmenu
+
