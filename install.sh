43a1874276c0187fd1d874e2f9907b2b30a96d41
Jazil T S <tsjazil@gmail.com>
2021-10-24 10:04:13 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 
 sudo wget -O /usr/local/bin/ktu-dmenu https://raw.githubusercontent.com/tsjazil/ktunotifications/main/ktunotifications-dmenu
-sudo wget -O /usr/local/bin/ktu-dmenu https://raw.githubusercontent.com/tsjazil/ktunotifications/main/ktunotifications-fzf
+sudo wget -O /usr/local/bin/ktu-fzf https://raw.githubusercontent.com/tsjazil/ktunotifications/main/ktunotifications-fzf
 echo Installing ...
 cd /usr/local/bin
 sudo chmod +x ktunotifications-fzf 
