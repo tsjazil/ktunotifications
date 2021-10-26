415b50b9855537fd7c00f8984c0b54344c04450a
tsjazil <tsjazil@gmail.com>
2021-10-26 09:29:25 +0530

    Updated Install Script

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-
+#Downloading scripts
 sudo wget -q -O /usr/local/bin/ktunotif-dmenu https://raw.githubusercontent.com/tsjazil/ktunotifications/main/ktunotif-dmenu
 sudo wget -q -O /usr/local/bin/ktunotif-fzf https://raw.githubusercontent.com/tsjazil/ktunotifications/main/ktunotif-fzf
-
+#executing scripts
 cd /usr/local/bin
-sudo chmod +x ktunotif-fzf 
+sudo chmod +x ktunotif-fzf
 sudo chmod +x ktunotif-dmenu
-
-
